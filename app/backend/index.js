46610b792c2084b5d309bba7f346eedd017374b7
tt201955 <tristan.tronconi@etu.univ-cotedazur.fr>
2026-02-10 14:36:45 +0100

    correction backend

--- app/backend/index.js
+++ app/backend/index.js
@@ -3,23 +3,28 @@ const app = express();
 const dotenv = require('dotenv');
 const connectDB = require("./config/db.js");
 dotenv.config();
-const port = process.env.PORT || 3000;
-connectDB();
+const port = process.env.PORT || 4000;
+const useDB = connectDB();
 
 const path = require("path");
-app.use(express.static(path.join(__dirname, "../frontend/*")));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/acceuil/index.html"));
-});
 
-app.get("/demineur", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/demineur/Jeu_demineur.html"));
-});
 
-app.get("/hack&slash", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/hack&slash/index.html"));
-});
+app.use(express.static(path.join(__dirname, "..", "frontend")));
+app.use(
+  "/",
+  express.static(path.join(__dirname, "..", "frontend", "acceuil"))
+);
+app.use(
+  "/demineur",
+  express.static(path.join(__dirname, "..", "frontend", "demineur"))
+);
+app.use(
+  "/hack&slash",
+  express.static(
+    path.join(__dirname, "..", "frontend", "hack&slash", "Hazard-Arena")
+  )
+);
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
